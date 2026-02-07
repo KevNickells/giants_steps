d6dac5ee768082406fdf02dfa7cb0b93600e0496
KevNickells <41780610+KevNickells@users.noreply.github.com>
2026-02-07 13:27:15 +0000

    Updating gitignore / flake setup

--- flake.nix
+++ flake.nix
@@ -1,24 +1,21 @@
 {
-  description = "some auld cack";
-
+  description = "giant steps microtonal chords";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
-
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-
       pythonEnv = pkgs.python312.withPackages (ps: with ps; [
           pyaudio
           numpy
-	  portaudio
       ]);
     in {
       devShells.${system}.default = pkgs.mkShell {
         packages = [
           pythonEnv
+          pkgs.portaudio
         ];
       };
     };
